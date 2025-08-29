285fba653af948b2f84eead1a611ca039010af86
igor-lima101 <igormaksym96@gmail.com>
2025-08-29 12:50:15 -0300

    create deposit and withdram, methods. Main class to test all features

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -27,4 +27,29 @@ public class BankAccount {
     public void setAmount(double amount) {
         this.amount = amount;
     }
+
+    public BankAccount(int number, String name, double amount){
+        this.number = number;
+        this.name = name;
+        this.amount = amount;
+    }
+
+    public void withdraw(double value){
+        if(getAmount() < 0 || getAmount() < value){
+            System.out.println("Error, insufficient founds");
+            return;
+        }
+        setAmount(getAmount() - value);
+        System.out.println("Transaction completed: withdraw -- [" + value + " R$]\n" + "Amount: [" + getAmount() + " R$]");
+    }
+
+    public void deposit(double value){
+        if(value <= 0){
+            System.out.println("Error, insufficient value");
+            return;
+        }
+
+        setAmount(getAmount() + value);
+        System.out.println("Transaction completed: deposit -- [" + value + " R$]\n" + "Amount: [" + getAmount() + " R$]");
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,15 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+
+        BankAccount bankAccount = new BankAccount(10, "igor", 2000.00);
+        BankAccount bankAccount1 = new BankAccount(20, "Marcos", 1000.00);
+        bankAccount.withdraw(500.00);
+        bankAccount1.deposit(230.00);
+
+        System.out.println("\n--- final amounts ---");
+        System.out.println(bankAccount.getName() + " | Account: " + bankAccount.getNumber() + " | Balance: " + String.format("%.2f R$", bankAccount.getAmount()));
+        System.out.println(bankAccount1.getName() + " | Account: " + bankAccount1.getNumber() + " | Balance: " + String.format("%.2f R$", bankAccount1.getAmount()));
     }
+
 }
